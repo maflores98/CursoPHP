2f13dbf2d29854e2efce0f016159c1235938cb2d
maflores98 <maflores98@prodigy.net.mx>
2017-07-11 21:00:25 -0500

    Variable super global
    $_GET[]
    toma el valor de la url y lo asigna a la variable en el codigo

--- MiFramework/index.php
+++ MiFramework/index.php
@@ -16,30 +16,23 @@
 <body>
 	<header>
 		<?php
-			// los comentarios en PHP inician con // o #
-			$Saludo = "Hola Mundo";
-			"<br>"
-			# 'Este es un saludo: $saludo' --> asi no imprime el saludo	
- 		?>
-		<h1>
-		<?php echo $Saludo; 
-					
-					echo "1.- Este es un saludo: $Saludo <br>";
-					# concatenar variables
-					echo '2.- Este es un saludo: ' . $Saludo . '<br>';
-					# forma explicita e imprimir una variable
-					echo "3.- Imprimiendo saludo: {$Saludo} <br>";
-				?>
+			// variable super global GET
+			$saludo = $_GET['saludo'];
+			$nombre = $_GET['nombre'];
+			$edad = $_GET['edad'];
+			 ?>
+			<h1> <?php echo $saludo . " " . $nombre;  ?> </h1>
+			<br>
+			<?php echo $edad;?> 
 			
-		</h1>
-
-		 
-    	<br><br>
-		<?php echo "Este es mi primer Framework";	
-		?>
+		
 	</header>
 
 </body>
 </html>
+
+<!-- url paraprobar este codigo : 
+http://miframework.com:8000/?saludo=Hola&nombre=Alejandro&edad=<h2>40</h2>
+-->
 
 
